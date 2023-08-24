208e776f1d833705a49c6df6b6e9fed2d28a047c
Mugesh Ram Sundar GS <138955449+Mugesh-Ram-Sundar@users.noreply.github.com>
2023-08-24 05:58:24 +0530

    20 min

---
+++ Adddatatodatabase.py
@@ -0,0 +1,82 @@
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
+
+cred = credentials.Certificate("serviceAccountKey.json")
+firebase_admin.initialize_app(cred,{
+    'databaseURL': "https://face-attendance-cv-default-rtdb.asia-southeast1.firebasedatabase.app/"
+})
+
+ref = db.reference('Students')
+
+data = {
+    "01dj":
+        {
+            "name": "Dwayne Johnson",
+            "major": "Mechanical",
+            "starting_year": "2021",
+            "total_attendance": "1",
+            "gpa": "9.1",
+            "year": "3",
+            "last_attendance_time": "00:00:00 01-01-2023"
+        },
+
+    "02ms":
+        {
+            "name": "Makato Shinkai",
+            "major": "Arts",
+            "starting_year": "2021",
+            "total_attendance": "2",
+            "gpa": "9.3",
+            "year": "2",
+            "last_attendance_time": "00:00:00 01-01-2023"
+        },
+
+    "03or":
+        {
+            "name": "Olivia Rodrigo",
+            "major": "Arts",
+            "starting_year": "2022",
+            "total_attendance": "3",
+            "gpa": "8.6",
+            "year": "1",
+            "last_attendance_time": "00:00:00 01-01-2023"
+        },
+
+    "04aa":
+        {
+            "name": "Afsal Ahmad",
+            "major": "Machine Learning",
+            "starting_year": "2022",
+            "total_attendance": "4",
+            "gpa": "8.4",
+            "year": "1",
+            "last_attendance_time": "00:00:00 01-01-2023"
+        },
+
+    "05mrs":
+        {
+            "name": "Mugesh Ram Sundar",
+            "major": "Machine Learning",
+            "starting_year": "2022",
+            "total_attendance": "1",
+            "gpa": "8.4",
+            "year": "1",
+            "last_attendance_time": "00:00:00 01-01-2023"
+        },
+
+    "06rc":
+        {
+            "name": "Ronnie Coleman",
+            "major": "Ece",
+            "starting_year": "2020",
+            "total_attendance": "3",
+            "gpa": "9.8",
+            "year": "4",
+            "last_attendance_time": "00:00:00 01-01-2023"
+        }
+
+}
+
+for key, value in data.items():
+    ref.child(key).set(value)
--- EncodeGenerator.py
+++ EncodeGenerator.py
@@ -2,6 +2,16 @@ import cv2
 import face_recognition
 import pickle
 import os
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
+from firebase_admin import storage
+
+cred = credentials.Certificate("serviceAccountKey.json")
+firebase_admin.initialize_app(cred,{
+    'databaseURL': "https://face-attendance-cv-default-rtdb.asia-southeast1.firebasedatabase.app/",
+    'storageBucket': "face-attendance-cv.appspot.com"
+})
 
 # import student images
 folderPath = 'Faces'
@@ -11,6 +21,12 @@ studentIds = []
 for path in PathList:
     imgList.append(cv2.imread(os.path.join(folderPath, path)))
     studentIds.append(os.path.splitext(path)[0])
+
+    fileName = f'{folderPath}/{path}'
+    bucket = storage.bucket()
+    blob = bucket.blob(fileName)
+    blob.upload_from_filename(fileName)
+
 print(studentIds)
 
 def findEncodings(imagesList):
